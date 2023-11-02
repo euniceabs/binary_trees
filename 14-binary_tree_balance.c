17fba1093c3e1075982c0d152d94ffe4fa8c7152
georgemayne <georgeegyirmayne07@gmail.com>
2023-11-02 12:19:55 -0700

    Binary Tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,13 +10,18 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height = 0;
 	size_t right_height = 0;
 
-	if (!tree)
+	if (tree == NULL)
+	{
 		return (0);
-
-	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-
-	return (left_height > right_height ? left_height : right_height);
+	}
+	else
+	{
+		if (tree)
+		{
+			left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		}
+	return ((left_height > right_height) ? left_height : right_height);
 }
 /**
  * binary_tree_balance - Measures the height of a binary tree.
