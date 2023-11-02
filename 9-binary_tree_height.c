a8de1af2a4eeacbf3f05beddb0aac19f1df4fe70
georgemayne <georgeegyirmayne07@gmail.com>
2023-11-02 08:26:12 -0700

    Binary Trees

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,12 +7,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height = 0;
+	size_t right_height = 0;
+
 	 if (tree == NULL)
 	 {
 		 return 0;
 	 }
-	 size_t left_height = binary_tree_height(tree->left);
-	 size_t right_height = binary_tree_height(tree->right);
+	 left_height = binary_tree_height(tree->left);
+	 right_height = binary_tree_height(tree->right);
 	 
 	 if (left_height > right_height)
 	 {
