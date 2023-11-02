a8d930fb7ceccd01241d07bdb43c2e10cf2efaa6
euniceabs <botweunice100@gmail.com>
2023-11-02 10:29:45 -0700

    correction

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - function that counts the leaves in a binary tree.
+ * binary_tree_leaves - function that counts the leaves in a binary tree.
  * @tree: Tree to count the leaves.
  * Return: 0 if tree is NULL.
  */
